0add8aead0069e0a352ad1f0a6755e335b31469d
Maneesh Chiba <mchiba@globeandmail.com>
2019-12-10 09:24:49 -0500

    Adds content script - Gets all internal links on page - Creates payload - Sends payload to background script

--- src/content.js
+++ src/content.js
@@ -1,1 +1,23 @@
-// This is the background script
+// This is the content script
+console.debug('🔗 Wiki Scout active');
+
+// Get all links on the page that are local
+const allLinks = document.querySelectorAll('a[href^="/"');
+
+// Transform the data
+const payload = [];
+[...allLinks].forEach((link) => {
+  if (link.innerText.length > 0){
+    payload.push ({
+      parent: window.location.href,
+      url: link.href,
+      text: link.innerText,
+    });
+  }
+});
+
+// Send the Data
+chrome.runtime.sendMessage({
+  type: 'LINKS_ON_PAGE',
+  data: payload
+})
