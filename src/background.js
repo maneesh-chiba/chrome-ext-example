29d04c7d83c7322806edfbbd8eec09a3808cbc02
Maneesh Chiba <mchiba@globeandmail.com>
2019-12-10 09:25:23 -0500

    Adds background script - Catches message from content script

--- src/background.js
+++ src/background.js
@@ -1,1 +1,15 @@
 // This is the background script
+// Listen for messages
+chrome.runtime.onMessage.addListener((message) => {
+  switch (message.type) {
+    case 'LINKS_ON_PAGE': {
+      console.log(message.data);
+    }
+      break;
+    default: {
+      console.error(`
+      Message ${message.type} received but is not handled
+      `)
+    }
+  }
+});
